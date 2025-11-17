ba8cdefd6668dcf640e3accd0103796347da689a
Julia Tsyganok <julia.tsyganok.9@yandex.ru>
2025-11-17 12:51:40 +0300

    Добавлены задачи

---
+++ src/Contain_duplicates.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == len(set(nums)):
+            return False
+        return True
+        
